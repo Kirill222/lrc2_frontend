8395af412b56ee4c0e7b09bbc3920dbdc6fc8540
johnconnor <zkmd2012@inbox.ru>
2021-10-07 14:59:55 +0500

    pagination component added from MUI library - minor change in UI (Next and Prev buttons do not work)

--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -1,4 +1,5 @@
 import { Typography, Grid } from "@material-ui/core"
+import { Pagination } from '@mui/material'
 import  Book  from './Book'
 import { useSelector } from "react-redux"
 import { useEffect } from "react"
@@ -6,47 +7,36 @@ import { getBooks, setTotalBookCount, setPageNumber } from "../../BLL/books_redu
 import { useDispatch } from "react-redux"
 import * as axios from 'axios'
 
-
-
 const Books = () => {
 
     const books = useSelector(state => state.books.books)
     const page = useSelector(state => state.books.page)
     const limit = useSelector(state => state.books.limit)
     const totalBookCount = useSelector(state => state.books.totalBookCount)
-
-    const numberOfPages = Math.ceil(totalBookCount / limit)
-    const pages = []
-    for (let i = 1; i <= numberOfPages; i++) {
-        pages.push(i)
-    }
-
-    console.log(pages)
-
     const dispatch = useDispatch()
+
+    const numberOfPages = Math.ceil(totalBookCount / limit)      
     
     useEffect(() => {
 
         const fetchedBooks = async () => {
-            let data = await axios.get(`http://localhost:5000/api/books?page=${page}&limit=${limit}`)
-
-            console.log(data.data)
+            let data = await axios.get(`http://localhost:5000/api/books?page=${page}&limit=${limit}`)            
 
             let books = data.data.results
             let totalCount = data.data.totalCount
             dispatch(getBooks(books))
             dispatch(setTotalBookCount(totalCount))
         } 
-        fetchedBooks() 
-        
+        fetchedBooks()         
     }, [books])
 
 
-    const onPageClick = (e) => {
-        console.log(Number(e.target.outerText))
+    const onChangeHandler = (e) => { 
+        console.log(e)
         dispatch(setPageNumber(Number(e.target.outerText)))
     }
 
+   
 
     return (
         <div>
@@ -61,13 +51,12 @@ const Books = () => {
                 }
             </Grid>
 
-            <div>
-               {
-                   pages.map(page => {
-                       return <a key={page} onClick={onPageClick}>{page}</a>
-                   })
-               }
-            </div>
+            <Pagination
+                onChange={onChangeHandler}
+                style={{display: "flex", justifyContent: "center"}}
+                variant="outlined"
+                count={numberOfPages}  
+            /> 
 
         </div>
         
