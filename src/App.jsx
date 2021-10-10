2096f2ec86f7982e9e2662144ba1a78908d06159
John Connor <zkmd2012@inbox.ru>
2021-10-10 16:36:52 +0500

    small improvement - home page added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import CssBaseline from '@material-ui/core/CssBaseline'
 import ResponsiveDrawer from './components/ResponsiveDrawer/ResponsiveDrawer'
 import {Switch, Route} from 'react-router-dom'
 import Books from './components/Books/Books'
+import HomePage from './components/HomePage/HomePage'
 import UploadBookForm from './components/Books/UploadBookForm'
 import BookDetailsPage from './components/Books/BookDetailsPage'
 import FormikForm from './components/Books/FormikForm'
@@ -14,11 +15,11 @@ function App() {
       <CssBaseline />     
       <ResponsiveDrawer>
         <Switch>
+          <Route exact path='/' component={HomePage}/>
           <Route path='/book/:bookId' component={BookDetailsPage}/>
           <Route path='/books' component={Books}/>
           <Route path='/upload' component={UploadBookForm}/>
           <Route path='/test' component={FormikForm}/>
-
         </Switch>
       </ResponsiveDrawer>
     </div>
