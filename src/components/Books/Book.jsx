9513f1c1b120391ed680cecec44e33ebecd24490
John Connor <zkmd2012@inbox.ru>
2021-10-10 16:44:39 +0500

    small UI improvements with grid

--- src/components/Books/Book.jsx
+++ src/components/Books/Book.jsx
@@ -26,7 +26,7 @@ export default function Book({book}) {
     }
  
     return (
-        <Grid item xs={12} sm={6} md={4} lg={2} key={book.id} >
+        <Grid item xs={12} sm={6} md={4} lg={3} xl={2} key={book.id} >
             <Card>
                 <CardActionArea>
                     <CardMedia
